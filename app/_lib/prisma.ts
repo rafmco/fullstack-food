e44a249823192c855d74c98080392420824db731
Rafael Machado <rafmco2@hotmail.com>
2024-04-30 16:59:26 -0400

    feat: add prisma file;

---
+++ app/_lib/prisma.ts
@@ -0,0 +1,22 @@
+/*
+  Arquivo para garantir que um prisma client seja instanciado em DEV
+*/
+
+/* eslint-disable no-unused-vars */
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var cachedPrisma: PrismaClient;
+}
+
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient();
+  }
+  prisma = global.cachedPrisma;
+}
+
+export const db = prisma;
