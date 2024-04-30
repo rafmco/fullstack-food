7c8b6b62ef7c74b8beb0826cad899bee4bba5f84
Rafael Machado <rafmco2@hotmail.com>
2024-04-30 16:56:46 -0400

    feat: add initial search component;

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
-import Image from "next/image";
 import Header from "./_components/header";
+import Search from "./_components/search";
 
-export default function Home() {
+const Home = () => {
   return (
-    <Header />
+    <>
+      <Header />
+      <div className="px-5">
+        <Search />
+      </div>
+    </>
   );
-}
+};
+
+export default Home;
