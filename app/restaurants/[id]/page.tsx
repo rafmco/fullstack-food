4cd3b8df8408d0ae986e5153afdc8c836d132631
Rafael Machado <rafmco2@hotmail.com>
2024-05-04 14:53:40 -0400

    feat: ensure restaurant can be favorited through restaurant page;

--- app/restaurants/[id]/page.tsx
+++ app/restaurants/[id]/page.tsx
@@ -6,6 +6,8 @@ import { StarIcon } from "lucide-react";
 import DeliveryInfo from "@/app/_components/delivery-info";
 import ProductList from "@/app/_components/product-list";
 import CartBanner from "./_components/cart-banner";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/_lib/auth";
 
 interface RestaurantPageProps {
   params: {
@@ -54,10 +56,20 @@ const RestaurantPage = async ({ params: { id } }: RestaurantPageProps) => {
   if (!restaurant) {
     return notFound();
   }
+  const session = await getServerSession(authOptions);
+
+  const userFavoriteRestaurants = await db.userFavoriteRestaurant.findMany({
+    where: {
+      userId: session?.user.id,
+    },
+  });
 
   return (
     <div>
-      <RestaurantImage restaurant={restaurant} />
+      <RestaurantImage
+        restaurant={restaurant}
+        userFavoriteRestaurants={userFavoriteRestaurants}
+      />
 
       <div className="relative z-50 mt-[-1.5rem] flex items-center justify-between rounded-tl-3xl rounded-tr-3xl bg-white px-5 pt-5">
         {/* TITULO */}
@@ -98,7 +110,7 @@ const RestaurantPage = async ({ params: { id } }: RestaurantPageProps) => {
 
       <div className="mt-6 space-y-4">
         {/* TODO: mostrar produtos mais pedidos quando implementarmos realização de pedido */}
-        <h2 className="px-5 font-semibold">Mais Pedidos</h2>
+        <h2 className="px-5  font-semibold">Mais Pedidos</h2>
         <ProductList products={restaurant.products} />
       </div>
 
